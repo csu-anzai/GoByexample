c39b9104b43061f7df2a87202d4c650c61ba8dec
7117 <qinglangsalaheiyou@163.com>
2019-09-02 22:16:16 +0800

    method

--- 19.method.go
+++ 19.method.go
@@ -1,47 +1,33 @@
-// Go 支持在结构体类型中定义方法 。
 package main
 
 import "fmt"
 
-// 接收器类型rect
 type rect struct {
 	width, height int
 }
 
-// 这里的 area 方法有一个接收器类型 rect。此处的接收器为r
-// 可以为值类型或者指针类型的接收器定义方法。这里是一个值类型接收器的例子。
-// func funcName(input1 type1, input2 type2) (otput1 type1, output2 type2) {
-// 		这里是处理逻辑代码
-// 		返回多个值
-// 		return value1, value2
-// }
-// 由此可以看出在普通的函数前面添加了一个接收器对象r
-// 这里是为接收器r的方法area 返回的是int的类型
 func (r *rect) area() int {
+	fmt.Println("area中r此时的状态",r);//area中r此时的状态 &{10 5}
 	return r.width * r.height
 }
-
-// 这里是为接收器r的方法perim 返回的是int的类型
 func (r rect) perim() int {
+	fmt.Println("perim中r此时的状态",r);//perim中r此时的状态 {10 5}
 	return 2*r.width + 2*r.height
 }
-
 func main() {
-	// r是一个对象
-	// 这个对象在不同的方法中 会有不同的结果
 	r := rect{width: 10, height: 5}
-	// 这里我们调用上面为结构体定义的两个方法。
-	fmt.Println("area: ", r.area())
-	fmt.Println("perim:", r.perim())
+	fmt.Println("main中r此时的状态",r);//&{10 5}
+	// r此时是地址 地址对应的width变量*地址对应的height变量
+	fmt.Println("带*的类型area: ", r.area())
+	// r此时是具体值 直接进行计算
+	fmt.Println("不带*的类型perim:", r.perim())
 
-	// Go 自动处理方法调用时的值和指针之间的转化。
-	// 你可以使用指针来调用方法来避免在方法调用时产生一个拷贝，或者让方法能够改变接受的数据。
+
 	rp := &r
-	fmt.Println("area: ", rp.area())
-	fmt.Println("perim:", rp.perim())
+	fmt.Println("rp此时的状态",rp);
+	// rp此时是地址 area()的r为地址  直接带入即可
+	fmt.Println("带*的类型area: ", rp.area())
+	// rp是地址  perim()的r为值  需要进行转换  找出地址对应的变量值
+	fmt.Println("不带*的类型perim:", rp.perim())
 }
 
-// area:  50
-// perim: 30
-// area:  50
-// perim: 30
