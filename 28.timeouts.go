41dccd743c5709674ff562cb5f13b17c3b5f6945
7117 <qinglangsalaheiyou@163.com>
2019-09-02 23:50:20 +0800

    select:选择执行

--- 28.timeouts.go
+++ 28.timeouts.go
@@ -10,25 +10,16 @@ func main() {
 		c1 <- "result 1"
 	}()
 
-	select {
-	case res := <-c1:
-		fmt.Println(res)
-	case <-time.After(time.Second * 1):
-		fmt.Println("timeout 1")
+	for i:=0;i<2;i++{
+		select {
+		case res := <-c1:
+			fmt.Println(res)
+		case <-time.After(time.Second * 1):
+			fmt.Println("timeout 1")
+		}
 	}
 
-	c2 := make(chan string, 1)
-	go func() {
-		time.Sleep(time.Second * 2)
-		c2 <- "result 2"
-	}()
-	select {
-	case res := <-c2:
-		fmt.Println(res)
-	case <-time.After(time.Second * 3):
-		fmt.Println("timeout 2")
-	}
 }
 
 // timeout 1
-// result 2
+// result 1
