1cdc643c6e9aac2c6b83867ef6ab812924499771
7117 <qinglangsalaheiyou@163.com>
2019-09-02 23:23:17 +0800

    channel:缓冲

--- 24.channel-buffering.go
+++ 24.channel-buffering.go
@@ -9,10 +9,11 @@ func main() {
 	// 这里我们 make 了一个通道，最多允许缓存 2 个值。
 	messages := make(chan string, 2)
 	// 因为这个通道是有缓冲区的，即使没有一个对应的并发接收方，我们仍然可以发送这些值。
-	messages <- "buffered"
-	messages <- "channel"
-	// 然后我们可以像前面一样接收这两个值。
+	messages <- "1"
 	fmt.Println(<-messages)
+
+	messages <- "2"
+	// 然后我们可以像前面一样接收这两个值。
 	fmt.Println(<-messages)
 }
 
