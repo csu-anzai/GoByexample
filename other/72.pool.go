dfc52172d9954a0dd10f1b168eec8ea7e58ce3db
7117 <qinglangsalaheiyou@163.com>
2019-09-03 19:50:59 +0800

    pool：中间件

--- other/72.pool.go
+++ other/72.pool.go
@@ -1,18 +1,36 @@
 package main
-
+ 
 import (
 	"log"
 	"sync"
 )
+ 
+// pool就是一个中间件
+// put放入
+// get取出
+// New为默认
 
 func main(){
 	// 建立对象
+	// var pipe = sync.Pool{
+	// 	New:func()interface{}{
+	// 		var a string
+	// 		a="aaaaaa"
+	// 		return &a
+	// 	},
+	// }
+
 	var pipe = &sync.Pool{
-		New:func()interface{}
-		{
-			return "Hello,BeiJing"
-		}
+		New: func() interface{} {
+			var mapp map[string]float32;
+			mapp = make(map[string]float32);
+			mapp["name"]=11.11;
+
+			return mapp
+		},
 	}
+
+
 	// 准备放入的字符串
 	val := "Hello,World!"
 	// 放入
@@ -22,6 +40,10 @@ func main(){
 	// 再取就没有了,会自动调用NEW
 	log.Println(pipe.Get())
 }
-
+//  有new的时候
 // 2018/12/29 17:50:10 Hello,World!
 // 2018/12/29 17:50:11 Hello,BeiJing
+
+// 没有new的时候
+// 2019/09/03 19:40:11 Hello,World!
+// 2019/09/03 19:40:11 <nil>
