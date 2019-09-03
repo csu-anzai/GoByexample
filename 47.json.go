c79fa327ba1bc6a3f59993b5855325f4ce13a0a9
7117 <qinglangsalaheiyou@163.com>
2019-09-03 23:14:24 +0800

    time

--- 47.json.go
+++ 47.json.go
@@ -4,11 +4,6 @@ import "encoding/json"
 import "fmt"
 import "os"
 
-// Go 提供内置的 JSON 编解码支持，
-// 包括内置或者自定义类型与 JSON 数据之间的转化。
-
-// 下面我们将使用这两个结构体来
-// 演示自定义类型的编码和解码。
 type Response1 struct {
 	Page   int
 	Fruits []string
