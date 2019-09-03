a15bf9f05f82770eaadee8af6d68f9fd9dd5a3a0
7117 <qinglangsalaheiyou@163.com>
2019-09-03 23:01:58 +0800

    字符串函数

--- 44.string-functions.go
+++ 44.string-functions.go
@@ -1,20 +1,12 @@
 package main
 
-// 字符串函数
-// 标准库的 strings 包提供了很多有用的字符串相关的函数。
-// 这里是一些用来让你对这个包有个初步了解的例子。
-
 import s "strings"
 import "fmt"
 
-// 我们给 fmt.Println 一个短名字的别名，我们随后将会经常用到。
 var p = fmt.Println
 
 func main() {
-	// 字符串的各种函数
-	// 这是一些 strings 中的函数例子。
-	// 注意他们都是包中的函数，不是字符串对象自身的方法，
-	// 这意味着我们需要考虑在调用时传递字符作为第一个参数进行传递。
+
 	p("Contains:  ", s.Contains("test", "es"))
 	p("Count:     ", s.Count("test", "t"))
 	p("HasPrefix: ", s.HasPrefix("test", "te"))
@@ -28,9 +20,7 @@ func main() {
 	p("ToLower:   ", s.ToLower("TEST"))
 	p("ToUpper:   ", s.ToUpper("test"))
 	p()
-	// 你可以在 strings包文档中找到更多的函数
-	// 虽然不是 strings 的一部分，
-	// 但是仍然值得一提的是获取字符串长度和通过索引获取一个字符的机制。
+
 	p("Len: ", len("hello"))
 	p("Char:", "hello"[1])
 }
