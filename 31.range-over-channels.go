298076b14357dca011d6d9067c95b6e8af743ff1
7117 <qinglangsalaheiyou@163.com>
2019-09-03 10:01:55 +0800

    channel:遍历

--- 31.range-over-channels.go
+++ 31.range-over-channels.go
@@ -1,20 +1,16 @@
 package main
 
-// 在前面的例子中，我们讲过 for 和 range为基本的
-// 数据结构提供了迭代的功能。我们也可以使用这
-// 个语法来遍历从通道中取得的值。
-
 import "fmt"
 
 func main() {
-	// 我们将遍历在 queue 通道中的两个值。
 	queue := make(chan string, 2)
 	queue <- "one"
 	queue <- "two"
 	close(queue)
-	// 这个 range 迭代从 queue 中得到的每个值。
-	// 因为我们在前面 close 了这个通道，这个迭代会在接收完 2 个值之后结束。
-	// 如果我们没有 close 它，我们将在这个循环中继续阻塞执行，等待接收第三个值
+
+	// close之后的通道也是可以遍历到的，这个遍历就相当于顺序的同步的读取channel
+	// 通道是没有index的
+	// 如果没有close的话  会堵塞  死锁  因为读写缺少一个并且没有协程  所以死锁
 	for elem := range queue {
 		fmt.Println(elem)
 	}
