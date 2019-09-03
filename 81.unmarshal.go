ac2b33490a9341526f2be02292d2e64434b85e96
7117 <qinglangsalaheiyou@163.com>
2019-09-03 22:40:17 +0800

    marshal与unmarshal

--- 81.unmarshal.go
+++ 81.unmarshal.go
@@ -3,6 +3,7 @@ package main
 import (
 	"encoding/json"
 	"fmt"
+	"reflect"
 )
 
 type Animal struct {
@@ -12,16 +13,23 @@ type Animal struct {
 }
 
 func main() {
+
 	var jsonBlob = []byte(`[
 		{"Name": "Platypus", "Order": "Monotremata"},
 		{"Name": "Quoll",    "Order": "Dasyuromorphia"}
 	]`)
+	a:=reflect.TypeOf(jsonBlob)
+	fmt.Println(a)
+	// 输出来的全部是ascii码
+	fmt.Println(jsonBlob)
+
 	var animals []Animal
 	// 会按照animala的方式进行编码
 	err := json.Unmarshal(jsonBlob, &animals)
 	if err != nil {
 		fmt.Println("error:", err)
 	}
+	// %+v会打印出键来
 	fmt.Printf("%+v", animals)
 }
 
